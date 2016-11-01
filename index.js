6b1c6769774c329dd1b9bfa6a676accd05a7c328
andrewgremlich <andrew.gremlich@gmail.com>
2016-11-01 10:49:49 -0600

    good filtering of images.

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ var fs = require('fs'),
     coursesRef = './resources/courses/';
 
 function base64_decode(base64str, file) {
-    var bitmap = new Buffer(base64str, 'base64');
-    fs.writeFileSync(file, bitmap);
-    console.log('******** File created from base64 encoded string ********');
+
+    var stringr = base64str.substring(0, 30);
+
+    if (stringr.includes("jpeg")) {
+        var replace = base64str.replace(/^data:image\/jpeg;base64,/, ""),
+            bitmap = new Buffer(replace, 'base64');
+        fs.writeFile(file + ".jpg", bitmap);
+        console.log('******** JPG created from base64 encoded string ********');
+    } else if (stringr.includes("png")) {
+        var replace = base64str.replace(/^data:image\/png;base64,/, ""),
+            bitmap = new Buffer(replace, 'base64');
+        fs.writeFile(file + ".png", bitmap);
+        console.log('******** PNG created from base64 encoded string ********');
+    } else {
+        console.log(file, " not and image bro!");
+    }
+
 }
 
 fs.readdir(coursesRef, (err, files) => {
@@ -18,7 +32,7 @@ fs.readdir(coursesRef, (err, files) => {
                     var data = jsondata[i];
                     for (var j in data) {
                         var banner = data[j];
-                        base64_decode(banner, `./images/${file.replace(".json", "")}_${j}.jpg`);
+                        base64_decode(banner, `./images/${file.replace(".json", "")}_${j}`);
                     }
                 }
             }
